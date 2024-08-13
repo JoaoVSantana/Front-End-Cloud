c8cb11262328787fd6a3e07c61d50c387f45c433
JoaoVSantana <joao10victorsantana@gmail.com>
2024-08-12 21:51:22 -0300

    Inserir imagens ainda não finalizado

--- src/app/[taskId]/page.tsx
+++ src/app/[taskId]/page.tsx
@@ -1,51 +1,54 @@
 'use client'
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams, useRouter } from "next/navigation";
 
-export default function Upload() {
-    const [file, setFile] = useState<File | null>(null);
-    const [taskId, setTaskId] = useState<string>("");
+interface File {
+    id: number;
+    url: string;
+}
 
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            setFile(e.target.files[0]);
+interface Task {
+    id: number;
+    titulo: string;
+}
+
+export default function TaskDetail() {
+    const [task, setTask] = useState<Task | null>(null);
+    const [images, setImages] = useState<string[]>([]);
+    const router = useRouter();
+    const id = useParams
+
+    useEffect(() => {
+        if (id) {
+            // Buscar a task específica
+            fetch(`http://localhost:8080/task/${id}`)
+                .then(response => response.json())
+                .then(data => setTask(data));
+
+            // Buscar as imagens associadas à task
+            fetch(`http://localhost:8080/aws/imagens/${id}`)
+                .then(response => response.json())
+                .then(data => setImages(data));
         }
-    };
+    }, [id]);
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-
-        if (!file) {
-            alert('Please select a file');
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        const response = await fetch(`http://localhost:8082/aws/${taskId}`, {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (response.ok) {
-            alert('File uploaded successfully');
-        } else {
-            alert('File upload failed');
-        }
-    };
+    if (!task) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
-            <h1 className='text-center text-2xl font-bold mb-6 p-2'>Upload File</h1>
-            <div className='flex flex-row gap-6 justify-center'>
-            <input
-                type="text"
-                value={taskId}
-                onChange={(e) => setTaskId(e.target.value)}
-                placeholder="Task ID"
-            />
-            <input type="file" onChange={handleFileChange} />
-            <button className='className="py-2 px-4 text-sm text-white rounded-lg bg-indigo-500 hover:bg-indigo-400 transition duration-200"' onClick={handleSubmit}>Upload</button>
+            <h1>{task.titulo}</h1>
+
+            <div>
+                <h2>Imagens</h2>
+                <div >
+                    {images.map((url, index) => (
+                        <div key={index}>
+                            <img src={url} alt={`Image ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
